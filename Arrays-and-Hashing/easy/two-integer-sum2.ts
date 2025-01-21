a4a7f2a899d28d09b4901b92064f0ab15597c812
DavidKielty1 <78271797+DavidKielty1@users.noreply.github.com>
2025-01-21 18:36:05 +0000

    Consistent strings sets

--- Arrays-and-Hashing/easy/two-integer-sum2.ts
+++ Arrays-and-Hashing/easy/two-integer-sum2.ts
@@ -1,17 +1,17 @@
-function twoSum(nums: number[], target: number): number[] {
+// function twoSum(nums: number[], target: number): number[] {
 
-    const map: Record<number, number> = {}
+//     const map: Record<number, number> = {}
 
-    for (let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i]
+//     for (let i = 0; i < nums.length; i++) {
+//         const complement = target - nums[i]
 
-        if (complement in map) {
-            return [i, map[complement]]
-        }
+//         if (complement in map) {
+//             return [i, map[complement]]
+//         }
 
-        map[nums[i]] = i
-    }
-    return
-};
+//         map[nums[i]] = i
+//     }
+//     return
+// };
 
-twoSum([1, 2, 3, 4], 5)
+// twoSum([1, 2, 3, 4], 5)
