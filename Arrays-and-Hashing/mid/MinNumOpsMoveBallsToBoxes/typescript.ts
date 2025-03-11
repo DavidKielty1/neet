4410b4e090ae1f94011a1d689df27253cb98edb3
David Kielty <davidkielty@live.co.uk>
2025-03-11 17:27:28 +0000

    Doing medium array challenge, all boxes in to each box

---
+++ Arrays-and-Hashing/mid/MinNumOpsMoveBallsToBoxes/typescript.ts
@@ -0,0 +1,26 @@
+// boxes [0, 0, 0, 1] - 4 boxes, last has a ball in it rest are empty
+
+
+const movingBalls = (balls: string) => {
+    console.log("Ma Balls!", balls)
+    const inputBallsArray = "110".split('').map(Number);
+
+    const output = [];
+
+    for(let i = 0; i < balls.length; i++) {
+
+        for(let j = 0; j < balls.length; j++) {
+
+            console.log(inputBallsArray[i]);
+            console.log(inputBallsArray[j]);
+
+            if (j === i) continue;
+            if (inputBallsArray[j] === 0) continue;
+            
+
+
+        }
+    }
+}
+
+movingBalls("110")
