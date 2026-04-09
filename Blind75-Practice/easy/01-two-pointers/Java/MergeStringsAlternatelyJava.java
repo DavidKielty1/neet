3bd3c2f9182b97b47db0733fe7652bb3f4b60f7c
David Kielty <davidkielty@live.co.uk>
2026-04-09 21:21:02 +0100

    Study

--- Blind75-Practice/easy/01-two-pointers/Java/MergeStringsAlternatelyJava.java
+++ Blind75-Practice/easy/01-two-pointers/Java/MergeStringsAlternatelyJava.java
@@ -11,21 +11,6 @@ Time: O(n), Space: O(n) for StringBuilder output
 public class MergeStringsAlternatelyJava {
 
     public String mergeAlternately(String word1, String word2) {
-        StringBuilder result = new StringBuilder();
-        int i = 0;
-        int j = 0;
-        while (i < word1.length() && j < word2.length()) {
-            result.append(word1.charAt(i++));
-            result.append(word2.charAt(j++));
-        }
-        if (i < word1.length()) {
-            result.append(word1.substring(i));
-        }
-        if (j < word2.length()) {
-            result.append(word2.substring(j));
-        }
-        return result.toString();
-
         //
 
         //
