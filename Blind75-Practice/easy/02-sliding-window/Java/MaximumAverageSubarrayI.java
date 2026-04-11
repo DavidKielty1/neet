bdca125aeb00c37bacb59778826fc14448ad3dbc
David Kielty <davidkielty@live.co.uk>
2026-04-11 13:48:16 +0100

    Study

--- Blind75-Practice/easy/02-sliding-window/Java/MaximumAverageSubarrayI.java
+++ Blind75-Practice/easy/02-sliding-window/Java/MaximumAverageSubarrayI.java
@@ -21,6 +21,24 @@
 public class MaximumAverageSubarrayI {
 
     public static double findMaxAverage(int[] nums, int k) {
+        if (nums == null || nums.length < k || k <= 0 || nums.length == 0) {
+            throw new IllegalArgumentException("Invalid Input"); 
+        }
+
+        int windowTotal = 0;
+        for ( int i = 0; i < k; i++) {
+            windowTotal += nums[i];
+        }
+
+        int maxAverage = windowTotal;
+        for (int i = k; i < nums.length; i++) {
+            windowTotal = windowTotal + nums[i] - nums[i - k];
+
+            maxAverage = Math.max(maxAverage, windowTotal);
+        }
+
+        return (double) maxAverage / k;
+
         //
 
         //
@@ -96,8 +114,6 @@ public class MaximumAverageSubarrayI {
             // }
         // }
         // return (double) maxSum / k;
-
-        throw new UnsupportedOperationException("Implement findMaxAverage");
     }
 
     public static void main(String[] args) {
